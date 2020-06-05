4c4e258e09e2d8e275ba05e8d2f2c8bda05371a9
buscaroli <m.buscaroli@outlook.com>
2020-06-05 15:11:01 +0100

    seasons: refactored if statement OUT of render() and added defaultProps.

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -20,17 +20,30 @@ class App extends React.Component {
         )
     }
     
+renderContent() {
+    if (this.state.errMsg && !this.state.lat) {
+        return <div>Error: {this.state.errMsg}</div>
+    }
+
+    if (!this.state.errMsg && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat}/>
+    }
+
+    return <Spinner message="Waiting for Location Request..."/>
+}
+
+    // The content that needs to be displayed has been refactored inside
+    // the renderContent function. This make it much easier to work on 
+    // it in case we wanted to change some of its properties (eg changing
+    // its css to give it a border) otherwise we would have had to change
+    // the properties in the three cases of the if statement.
+    // LESSON: AVOID CONDITIONALS WITHIN RENDER()
     render() {
-        if (this.state.errMsg && !this.state.lat) {
-            return <div>Error: {this.state.errMsg}</div>
-        }
-
-        if (!this.state.errMsg && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat}/>
-        }
-
-        return <Spinner />
-       
+       return (
+           <div>
+               {this.renderContent()}
+           </div>  
+       )
     }
 }
 
