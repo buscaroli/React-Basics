3b4c4fe0eb26f8c757dd24c38b604e8171a9fcd0
buscaroli <m.buscaroli@outlook.com>
2020-06-11 15:49:30 +0100

    vids: added VideoItem Component and CSS file. Showing list of youtube videos.

--- vids/src/components/VideoList.js
+++ vids/src/components/VideoList.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import VideoItem from './VideoItem'
 
 
 const VideoList = props => {
+    const renderedList = props.videos.map((x) => {
+        return <VideoItem video={x} />
+    })
+    
     return (
-        <div>
-            {props.videos.length}
+        <div className="ui relaxed divided list">
+            {renderedList}
         </div>
     )
 }
